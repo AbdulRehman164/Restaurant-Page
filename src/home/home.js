97fbeeda9a4a3f9172c8625c3083e1c8c7a0dc52
Abdul Rehman <abdulrehmang571@gmail.com>
2023-06-23 16:02:18 +0500

    wrote a function in the getHome function that returns the article mainHome which contains heading intro and image

--- src/home/home.js
+++ src/home/home.js
@@ -1,4 +1,27 @@
+import burger from './burger.png';
+
 export default function getHome() {
   const content = document.querySelector('#content');
   content.innerHTML = '';
+
+  function getMainHome() {
+    const mainHome = document.createElement('article');
+    mainHome.setAttribute('id', 'mainHome');
+
+    const introduction = document.createElement('section');
+    introduction.classList.add('introduction');
+    const restaurantHeading = document.createElement('h1');
+    restaurantHeading.textContent = 'Star Bites';
+    const introPara = document.createElement('p');
+    introPara.textContent =
+      'Welcome to Star Bites, a culinary paradise that promises to take your taste buds on an extraordinary journey! Nestled in the heart of London, our restaurant is renowned for its exquisite flavors, warm ambiance, and unparalleled dining experience.';
+    introduction.append(restaurantHeading, introPara);
+
+    const burgerImage = document.createElement('img');
+    burgerImage.src = burger;
+    mainHome.append(introduction, burgerImage);
+    return mainHome;
+  }
+
+  content.appendChild(getMainHome());
 }
