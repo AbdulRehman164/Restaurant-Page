a3c906d2eb2d66ee81cbd284515669af291ce523
Abdul Rehman <abdulrehmang571@gmail.com>
2023-06-24 14:49:43 +0500

    append the menuheading in the content

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,4 +1,8 @@
 export default function getMenu() {
   const content = document.querySelector('#content');
   content.innerHTML = '';
+  const menuHeading = document.createElement('h1');
+  menuHeading.textContent = 'Menu';
+
+  content.append(menuHeading);
 }
