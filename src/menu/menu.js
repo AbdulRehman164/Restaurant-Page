0deccabe284c5d2734a83fe39f412c8e7189186e
Abdul Rehman <abdulrehmang571@gmail.com>
2023-06-24 15:25:08 +0500

    to create the menuCards created a factory function which returns an object and using that object menu cards are created which were first hard coded

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -8,10 +8,7 @@ export default function getMenu() {
   const menuHeading = document.createElement('h1');
   menuHeading.textContent = 'Menu';
 
-  const appetizerHeading = document.createElement('h2');
-  appetizerHeading.textContent = 'Appetizers';
-  function getAppetizerSection() {
-    const section = document.createElement('section');
+  function getMenuCards() {
     const menuCards = [];
     const menuCardsHeading = [];
     const menuCardsPara = [];
@@ -35,27 +32,43 @@ export default function getMenu() {
         menuCardsDiv[i]
       );
     }
+    return {
+      menuCards,
+      menuCardsHeading,
+      menuCardsPara,
+      menuCardsDiv,
+      menuCardsPricePara,
+      menuCardsImg,
+    };
+  }
 
-    menuCardsHeading[0].textContent = 'Bruschetta';
-    menuCardsHeading[1].textContent = 'Artichoke Dip';
-    menuCardsHeading[2].textContent = 'Chicken Satay Skewers';
+  const appetizerHeading = document.createElement('h2');
+  appetizerHeading.textContent = 'Appetizers';
+  function getAppetizerSection() {
+    const section = document.createElement('section');
 
-    menuCardsPara[0].textContent =
+    const card = getMenuCards();
+
+    card.menuCardsHeading[0].textContent = 'Bruschetta';
+    card.menuCardsHeading[1].textContent = 'Artichoke Dip';
+    card.menuCardsHeading[2].textContent = 'Chicken Satay Skewers';
+
+    card.menuCardsPara[0].textContent =
       'Toasted baguette slices topped with fresh tomatoes, basil, and a drizzle of balsamic glaze.';
-    menuCardsPara[1].textContent =
+    card.menuCardsPara[1].textContent =
       'Creamy blend of spinach, artichoke hearts, and melted cheese, served with crispy tortilla chips.';
-    menuCardsPara[2].textContent =
+    card.menuCardsPara[2].textContent =
       'Grilled chicken skewers marinated in a flavorful blend of spices, accompanied by a tangy peanut sauce.';
 
-    menuCardsPricePara[0].textContent = '3$';
-    menuCardsPricePara[1].textContent = '5$';
-    menuCardsPricePara[2].textContent = '6$';
+    card.menuCardsPricePara[0].textContent = '3$';
+    card.menuCardsPricePara[1].textContent = '5$';
+    card.menuCardsPricePara[2].textContent = '6$';
 
-    menuCardsImg[0].src = bruschetta;
-    menuCardsImg[1].src = artichoke;
-    menuCardsImg[2].src = skewers;
+    card.menuCardsImg[0].src = bruschetta;
+    card.menuCardsImg[1].src = artichoke;
+    card.menuCardsImg[2].src = skewers;
 
-    section.append(...menuCards);
+    section.append(...card.menuCards);
 
     return section;
   }
