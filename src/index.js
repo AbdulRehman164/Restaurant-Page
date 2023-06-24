a37f59c25277e6c56dc291e9621aafe454b1236f
Abdul Rehman <abdulrehmang571@gmail.com>
2023-06-24 17:30:46 +0500

    created a navBar function which creates the navbar

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import getHome from './home/home';
 import getMenu from './menu/menu';
 import './style.css';
 
-getHome();
-getMenu();
+function navigation() {
+  const navbar = document.querySelector('#navbar');
+  const logoDiv = document.createElement('div');
+  const optionsDiv = document.createElement('div');
+  const home = document.createElement('div');
+  const menu = document.createElement('div');
+  const about = document.createElement('div');
+
+  logoDiv.textContent = 'Star Bites';
+  home.textContent = 'Home';
+  menu.textContent = 'Menu';
+  about.textContent = 'About';
+
+  logoDiv.classList.add('navItem');
+  optionsDiv.classList.add('option', 'navItem');
+
+  optionsDiv.append(home, menu, about);
+
+  navbar.append(logoDiv, optionsDiv);
+}
+navigation();
