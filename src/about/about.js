c9008022173cdd5fe6664aed28cf2fbc9fa406a7
Abdul Rehman <abdulrehmang571@gmail.com>
2023-06-25 13:47:10 +0500

    added contact us heading on the about page

--- src/about/about.js
+++ src/about/about.js
@@ -1,4 +1,9 @@
 export default function getAbout() {
   const content = document.querySelector('#content');
   content.innerHTML = '';
+
+  const aboutHeading = document.createElement('h1');
+  aboutHeading.textContent = 'Contect us';
+
+  content.append(aboutHeading);
 }
